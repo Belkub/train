17c76fc327a4d6241952b31209d015951aa2e84d
Евгений <137326840+Belkub@users.noreply.github.com>
2025-06-01 16:57:16 +0300

    Add files via upload

---
+++ pages/Детергенты.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Противосальниковые добавки")
+st.sidebar.success("Детергенты")
+#col1, col2 = st.columns(2)
+ing_1 = st.checkbox('Противосальниковая композиция неионных ПАВ')
+ing_2 = st.checkbox('Противосальиковый и ингибирующий комплекс на основе окисленных аминов')
+
+df = pd.read_excel('D:\\Users\\файлы\\train\\pages\\База реагенты.xlsx')
+
+if ing_1:
+    st.error(df['Название'][14])
+    st.warning(df['Описание'][14])
+    st.success(df['Цена'][14])
+if ing_2:
+    st.error(df['Название'][15])
+    st.warning(df['Описание'][15])
+    st.success(df['Цена'][15])
