e7ff2fba8d303e6cda9c699cef2c136f2e5def61
Евгений <137326840+Belkub@users.noreply.github.com>
2025-06-01 16:47:55 +0300

    Add files via upload

---
+++ Старт.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title = "This is a Multipage WebApp")
+st.title("Каталог химических реагентов для буровых растворов. Более 15 лет опыта производства и поставки реагентов для месторождений РФ, Узбекситана и Казахстана")
+st.sidebar.success("Выбрать категорию")
